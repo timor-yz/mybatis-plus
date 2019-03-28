1633f00b8a122658ba967aa597c7ed5ac04753f4
Timor <yuanzhe13579@163.com>
2019-03-28 09:17:09 +0800

    优化

--- src/main/java/com/timor/mybatisplus/generator/CodeGenerator.java
+++ src/main/java/com/timor/mybatisplus/generator/CodeGenerator.java
@@ -16,15 +16,6 @@ import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
  */
 public class CodeGenerator
 {
-	/**
-	 * @Fields tables : 需生成文件对应的表名数组（按需更变）
-	 */
-	private static final String[] TABLE_NAMES = new String[] { "wo_close", "wo_detail" };
-
-	/**
-	 * @Fields tables : 生成文件所在包名（按需更变）
-	 */
-	private static final String PACKAGE_NAME = "com.timor";
 
 	public static void main(String[] args)
 	{
@@ -33,17 +24,16 @@ public class CodeGenerator
 
 		/*-- 全局配置 --*/
 		GlobalConfig gc = new GlobalConfig();
-		gc.setOutputDir("D://CodeGenerator//");
-		gc.setAuthor("YuanZhe");
+		gc.setOutputDir(ConfigInfo.OUTPUT_DIR);
+		gc.setAuthor(ConfigInfo.AUTHOR);
 		mpg.setGlobalConfig(gc);
 
 		/*-- 数据源配置 --*/
-		// TODO 按需更变
 		DataSourceConfig dsc = new DataSourceConfig();
-		dsc.setUrl("");
-		dsc.setDriverName("com.mysql.jdbc.Driver");
-		dsc.setUsername("");
-		dsc.setPassword("");
+		dsc.setUrl(ConfigInfo.URL);
+		dsc.setDriverName(ConfigInfo.DRIVER_NAME);
+		dsc.setUsername(ConfigInfo.USERNAME);
+		dsc.setPassword(ConfigInfo.PASSWORD);
 		mpg.setDataSource(dsc);
 
 		/*-- 数据库表配置 --*/
@@ -51,13 +41,13 @@ public class CodeGenerator
 		strategy.setNaming(NamingStrategy.underline_to_camel);
 		strategy.setColumnNaming(NamingStrategy.underline_to_camel);
 		strategy.setRestControllerStyle(true);
-		strategy.setInclude(TABLE_NAMES);
+		strategy.setInclude(ConfigInfo.TABLE_NAMES);
 		strategy.setControllerMappingHyphenStyle(true);
 		mpg.setStrategy(strategy);
 
 		/*-- 包配置 --*/
 		PackageConfig pc = new PackageConfig();
-		pc.setParent(PACKAGE_NAME);
+		pc.setParent(ConfigInfo.PACKAGE_NAME);
 		mpg.setPackageInfo(pc);
 
 		/*-- 采用 Freemarker 模板引擎（默认 Velocity） --*/
---
+++ src/main/java/com/timor/mybatisplus/generator/ConfigInfo.java
@@ -0,0 +1,50 @@
+package com.timor.mybatisplus.generator;
+
+/**
+ * @Description 配置信息
+ * @author YuanZhe
+ * @date 2019年3月28日 上午8:56:35
+ * 
+ */
+public class ConfigInfo
+{
+	/**
+	 * @Fields tables : 生成文件所在目录（按需更变）
+	 */
+	public static final String OUTPUT_DIR = "D://CodeGenerator//";
+	
+	/**
+	 * @Fields tables : 作者（按需更变）
+	 */
+	public static final String AUTHOR = "YuanZhe";
+
+	/**
+	 * @Fields tables : 需生成文件对应的表名数组（按需更变）
+	 */
+	public static final String[] TABLE_NAMES = new String[] { "" };
+
+	/**
+	 * @Fields tables : 生成文件所在包名（按需更变）
+	 */
+	public static final String PACKAGE_NAME = "";
+
+	/**
+	 * @Fields tables : 连接数据库所需URL（按需更变）
+	 */
+	public static final String URL = "";
+
+	/**
+	 * @Fields tables : 连接数据库驱动名称（按需更变）
+	 */
+	public static final String DRIVER_NAME = "";
+
+	/**
+	 * @Fields tables : 连接数据库所需用户名（按需更变）
+	 */
+	public static final String USERNAME = "";
+
+	/**
+	 * @Fields tables : 连接数据库所需密码（按需更变）
+	 */
+	public static final String PASSWORD = "";
+}
