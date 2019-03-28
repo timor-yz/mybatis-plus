925b1ba21d7c89e3ad596cc536f77e569b885018
Timor <yuanzhe13579@163.com>
2019-03-28 09:20:14 +0800

    优化

--- src/main/java/com/timor/mybatisplus/generator/ConfigInfo.java
+++ src/main/java/com/timor/mybatisplus/generator/ConfigInfo.java
@@ -9,42 +9,42 @@ package com.timor.mybatisplus.generator;
 public class ConfigInfo
 {
 	/**
-	 * @Fields tables : 生成文件所在目录（按需更变）
+	 * @Fields tables : 生成文件所在目录
 	 */
 	public static final String OUTPUT_DIR = "D://CodeGenerator//";
 	
 	/**
-	 * @Fields tables : 作者（按需更变）
+	 * @Fields tables : 作者
 	 */
 	public static final String AUTHOR = "YuanZhe";
 
 	/**
-	 * @Fields tables : 需生成文件对应的表名数组（按需更变）
+	 * @Fields tables : 需生成文件对应的表名数组
 	 */
 	public static final String[] TABLE_NAMES = new String[] { "" };
 
 	/**
-	 * @Fields tables : 生成文件所在包名（按需更变）
+	 * @Fields tables : 生成文件所在包名
 	 */
 	public static final String PACKAGE_NAME = "";
 
 	/**
-	 * @Fields tables : 连接数据库所需URL（按需更变）
+	 * @Fields tables : 连接数据库所需URL
 	 */
 	public static final String URL = "";
 
 	/**
-	 * @Fields tables : 连接数据库驱动名称（按需更变）
+	 * @Fields tables : 连接数据库驱动名称
 	 */
 	public static final String DRIVER_NAME = "";
 
 	/**
-	 * @Fields tables : 连接数据库所需用户名（按需更变）
+	 * @Fields tables : 连接数据库所需用户名
 	 */
 	public static final String USERNAME = "";
 
 	/**
-	 * @Fields tables : 连接数据库所需密码（按需更变）
+	 * @Fields tables : 连接数据库所需密码
 	 */
 	public static final String PASSWORD = "";
 }
